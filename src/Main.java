b86ca8d64ce6ab6e8e23316b6c63df6ad0ab2ad5
vimal kumar <vimal.kumar@imzcorporate.com>
2024-04-05 16:30:43 +0530

    data types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+//data types
+public class Main{
+    public static void main(String[] args){
+        int age=20;
+        float weight=700.56f;
+        double number=11239182734.329847;
+        String name="vimal";
+        char letter='A';
+        System.out.println("all the datatypes are"+age+weight+name+letter+number);
     }
 }
