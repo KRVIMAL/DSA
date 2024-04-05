1a942fe486dce84c3e3a43643a7df406f4745358
vimal kumar <vimal.kumar@imzcorporate.com>
2024-04-05 17:13:40 +0530

    loops

---
+++ src/Loops/Loops.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class Loops {
+    public static void main(String[] args) {
+//        for(int i =1;i<=10;i++)
+//        System.out.println(i);
+        int i=1;
+        while (i<=10){
+            System.out.println(i);
+            i++;
+        }
+        int age=18;
+        if(age>=18){
+            System.out.println("you can vote");
+        }
+        else {
+            System.out.println("you cannot vote");
+        }
+    }
+}
